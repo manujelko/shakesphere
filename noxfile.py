470b9079871eb9d4c12dd9dc4cd9e671d5fd5138
Manuel Saric <manujelko@gmail.com>
2020-07-18 14:01:31 +0200

    Add integration tests for description urls.

--- noxfile.py
+++ noxfile.py
@@ -5,4 +5,11 @@ import nox
 def pytest(session):
     session.install("-e", ".")
     session.install("-r", "requirements/dev.txt")
-    session.run("pytest", "--cov")
+    session.run("pytest", "--cov", "-m", "not integration")
+
+
+@nox.session(python=["3.8", "3.7"])
+def pytest_integration(session):
+    session.install("-e", ".")
+    session.install("-r", "requirements/dev.txt")
+    session.run("pytest", "-m", "integration")
--- tests/test_pokemon.py
+++ tests/test_pokemon.py
@@ -15,3 +15,15 @@ def mock_species_url(mocker):
 def test_get_pokemon_species_url(mock_species_url) -> None:
     url = get_pokemon_species_url("fake pokemon")
     assert url == "Totally fake url"
+
+
+@pytest.mark.integration
+@pytest.mark.parametrize("pokemon_name, expected_url", [
+    pytest.param("pikachu", "https://pokeapi.co/api/v2/pokemon-species/25/"),
+    pytest.param("bulbasaur", "https://pokeapi.co/api/v2/pokemon-species/1/"),
+    pytest.param("squirtle", "https://pokeapi.co/api/v2/pokemon-species/7/"),
+    pytest.param("charmander", "https://pokeapi.co/api/v2/pokemon-species/4/"),
+])
+def test_get_pokemon_species_url_integration(pokemon_name, expected_url) -> None:
+    url = get_pokemon_species_url(pokemon_name)
+    assert url == expected_url, f"Unexpected url found: {url}, {expected_url} was expected"
