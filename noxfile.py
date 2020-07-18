d2ff30982f13b1953896fc4900acaf4cc5d7b0dc
Manuel Saric <manujelko@gmail.com>
2020-07-18 14:28:30 +0200

    Use a single requirements file

--- noxfile.py
+++ noxfile.py
@@ -7,14 +7,16 @@ locations = "src", "tests", "noxfile.py"
 @nox.session(python=["3.8", "3.7"])
 def pytest(session):
     session.install("-e", ".")
-    session.install("-r", "requirements/dev.txt")
+    session.install("-r", "requirements.txt")
+    session.install("pytest", "pytest-mock", "pytest-cov")
     session.run("pytest", "--cov", "-m", "not integration")
 
 
 @nox.session(python=["3.8", "3.7"])
 def integration(session):
     session.install("-e", ".")
-    session.install("-r", "requirements/dev.txt")
+    session.install("-r", "requirements.txt")
+    session.install("pytest")
     session.run("pytest", "-m", "integration")
 
 
