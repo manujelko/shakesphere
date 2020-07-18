056288ce035c9f1553b65d021b0b88ca572ab010
Manuel Saric <manujelko@gmail.com>
2020-07-18 14:07:24 +0200

    Add flake8 for linting.

--- noxfile.py
+++ noxfile.py
@@ -1,6 +1,9 @@
 import nox
 
 
+locations = "src", "tests", "noxfile.py"
+
+
 @nox.session(python=["3.8", "3.7"])
 def pytest(session):
     session.install("-e", ".")
@@ -13,3 +16,10 @@ def pytest_integration(session):
     session.install("-e", ".")
     session.install("-r", "requirements/dev.txt")
     session.run("pytest", "-m", "integration")
+
+
+@nox.session(python=["3.8", "3.7"])
+def lint(session):
+    args = session.posargs or locations
+    session.install("flake8")
+    session.run("flake8", *args)
