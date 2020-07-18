14b98254c2e41a8a516267bbc90589fc3fa3f8d7
Manuel Saric <manujelko@gmail.com>
2020-07-18 12:50:40 +0200

    Make package installable.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Shakesphere
+
+An API written in Python used for retrieving Shakespearean pokemon descriptions.
---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as stream:
+    long_description = stream.read()
+
+
+setup(
+    name="shakesphere",
+    version="0.1.0",
+    author="Manuel Saric",
+    author_email="manujelko@gmail.com",
+    description="Shakespearean Pokemon descriptions API.",
+    long_description=long_description,
+    url="https://packaging.python.org/tutorials/packaging-projects/",
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+)
