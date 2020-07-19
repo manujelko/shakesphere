66962416eaec6675b3bfc7a6f46790ba6b48d115
Manuel Saric <manujelko@gmail.com>
2020-07-19 09:26:19 +0200

    Add docstrings to tests.

--- tests/test_pokemon.py
+++ tests/test_pokemon.py
@@ -20,16 +20,19 @@ def mock_requests(mocker):
 
 
 def test_get_pokemon_species_url(mock_requests) -> None:
+    """It gets the url from the correct key."""
     url = get_pokemon_species_url("fake_pokemon")
     assert url == "fake_species_url"
 
 
 def test_get_descriptions(mock_requests) -> None:
+    """It gets the descriptions in english."""
     descriptions = get_descriptions("fake_url")
     assert descriptions[0] == "fake_description_en"
 
 
 def test_get_random_pokemon_description(mock_requests) -> None:
+    """It gets a description."""
     description = get_random_pokemon_description("fake_pokemon")
     assert description == "fake_description_en"
 
@@ -45,6 +48,7 @@ def test_get_random_pokemon_description(mock_requests) -> None:
     ],
 )
 def test_get_pokemon_species_url_integration(pokemon_name, expected_url) -> None:
+    """It gets the expected urls."""
     url = get_pokemon_species_url(pokemon_name)
     assert (
         url == expected_url
@@ -63,6 +67,7 @@ def test_get_pokemon_species_url_integration(pokemon_name, expected_url) -> None
     ids=["pikachu", "bulbasaur", "squirtle", "charmander"],
 )
 def test_get_descriptions_integration(url) -> None:
+    """It gets a list of descriptions."""
     descriptions = get_descriptions(url)
     assert descriptions, "Expected a list of descriptions"
 
@@ -72,5 +77,6 @@ def test_get_descriptions_integration(url) -> None:
     "pokemon_name", ["pikachu", "bulbasaur", "squirtle", "charmander"]
 )
 def test_get_random_pokemon_description_integration(pokemon_name) -> None:
+    """It gets a description."""
     description = get_random_pokemon_description(pokemon_name)
     assert description, "Expected a pokemon description"
