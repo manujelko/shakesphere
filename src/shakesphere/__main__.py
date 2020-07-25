90dd76072e8265c2631a8cc31b5fe863c1e07916
Manuel Saric <manujelko@gmail.com>
2020-07-25 10:07:31 +0200

    Prepare docker image

--- src/shakesphere/__main__.py
+++ src/shakesphere/__main__.py
@@ -1,7 +1,14 @@
+import sys
+
 from bottle import run
 
 from .app import app
 
 
 if __name__ == "__main__":
-    run(app, host="localhost", port=8080)
+    if len(sys.argv) == 1:
+        run(app, host="localhost", port=8080)
+    elif len(sys.argv) == 2 and sys.argv[1] == "--docker":
+        run(app, host="0.0.0.0", port=8000)
+    else:
+        raise RuntimeError("--docker is the only allowed argument")
